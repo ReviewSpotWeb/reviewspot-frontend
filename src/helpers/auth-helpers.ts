77587be93980630dfbabf40c0cdd9a27b2a49249
Eli MacColl <elimaccoll@gmail.com>
2023-11-05 16:46:47 -0500

    style: add comment

--- src/helpers/auth-helpers.ts
+++ src/helpers/auth-helpers.ts
@@ -1,6 +1,7 @@
 import { ToastPosition } from "react-toastify";
 import { ToastInfo, showToastMessage } from "./toast-helpers";
 
+// TODO: just use showToastMessage wherever I used this
 export const loginToast = (message: string, position: ToastPosition) => {
   const toastInfo: ToastInfo = {
     message: message,
